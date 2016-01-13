435ea0842d2092a3e0366ce7c284e0311e3e21eb
Adam Hinz <hinz.adam@gmail.com>
2016-01-12 22:44:03 -0500

    Initial marker controls

---
+++ code/maps/Marker.elm
@@ -0,0 +1,12 @@
+module Maps.Marker where
+
+import Maps.Core as M
+import Maps.Util as U
+
+import Maps.Geometry exposing (..)
+import Maps.Geography exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (style, class, id, src)
+
+a = 2
